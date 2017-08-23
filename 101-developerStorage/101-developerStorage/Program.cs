98b179ab10e9dd4846200b0fdb1d92156aadccf8
Børhaug <Brede.Borhaug@dnvgl.com>
2017-08-23 20:26:41 +0200

    Added methods to create SAS URI for containers and blobs

--- 101-developerStorage/101-developerStorage/Program.cs
+++ 101-developerStorage/101-developerStorage/Program.cs
@@ -17,15 +17,63 @@ namespace _101_developerStorage
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
 
             //Get a reference to a container to use for the sample code, and create it if it does not exist.
-            CloudBlobContainer container = blobClient.GetContainerReference("sascontainer");
+            CloudBlobContainer container = blobClient.GetContainerReference("veracitycontainer");
             container.CreateIfNotExists();
 
             //Insert calls to the methods created below here...
 
-
+            //Generate a SAS URI for the container, without a stored access policy.
+            Console.WriteLine("Container SAS URI: " + GetContainerSasUri(container));
+            Console.WriteLine();
+            //Generate a SAS URI for a blob within the container, without a stored access policy.
+            Console.WriteLine("Blob SAS URI: " + GetBlobSasUri(container));
+            Console.WriteLine();
 
             //Require user input before closing the console window.
             Console.ReadLine();
         }
+
+        // Lets create a method that will create the SAS URI for the container
+        static string GetContainerSasUri(CloudBlobContainer container)
+        {
+            //Set the expiry time and permissions for the container.
+            //In this case no start time is specified, so the shared access signature becomes valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.List | SharedAccessBlobPermissions.Write;
+
+            //Generate the shared access signature on the container, setting the constraints directly on the signature.
+            string sasContainerToken = container.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return container.Uri + sasContainerToken;
+        }
+
+        // Lets create the method that will create the SAS URI for a Blob
+        static string GetBlobSasUri(CloudBlobContainer container)
+        {
+            //Get a reference to a blob within the container.
+            CloudBlockBlob blob = container.GetBlockBlobReference("veracityblob.txt");
+
+            //Upload text to the blob. If the blob does not yet exist, it will be created.
+            //If the blob does exist, its existing content will be overwritten.
+            string blobContent = "This blob will be accessible to clients via a shared access signature (SAS). This is similar to how the clients will read data from Veracity.";
+            blob.UploadText(blobContent);
+
+            //Set the expiry time and permissions for the blob.
+            //In this case, the start time is specified as a few minutes in the past, to mitigate clock skew.
+            //The shared access signature will be valid immediately.
+            SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+            sasConstraints.SharedAccessStartTime = DateTimeOffset.UtcNow.AddMinutes(-5);
+            sasConstraints.SharedAccessExpiryTime = DateTimeOffset.UtcNow.AddHours(24);
+            sasConstraints.Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write;
+
+            //Generate the shared access signature on the blob, setting the constraints directly on the signature.
+            string sasBlobToken = blob.GetSharedAccessSignature(sasConstraints);
+
+            //Return the URI string for the container, including the SAS token.
+            return blob.Uri + sasBlobToken;
+        }
+
     }
 }
