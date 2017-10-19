6fc911a82d4dc7808a6a16844a31eda25ef3c3c0
Børhaug <Brede.Borhaug@dnvgl.com>
2017-10-19 09:11:48 +0200

    renamed 101-security

---
+++ 101-egest-data/egest-data-java/egestdata.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+// We need the following imports to use blob APIs
+ import com.microsoft.azure.storage.*;
+// import com.microsoft.azure.storage.blob.*;
+
+
+public class egestdata {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
