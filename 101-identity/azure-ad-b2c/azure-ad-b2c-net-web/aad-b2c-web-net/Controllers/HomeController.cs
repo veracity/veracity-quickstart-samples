fd8eafbbccc5d361e1fc6af2e675ea29ffa19905
Pawel Lehmann <Pawel.Lehmann@dnvgl.com>
2017-10-20 10:32:46 +0200

    Add sample code for aad b2c in .net web application

---
+++ 101-identity/azure-ad-b2c/azure-ad-b2c-net-web/aad-b2c-web-net/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Mvc;
+
+namespace aad_b2c_web_net.Controllers
+{
+    /// <summary>
+    /// Based on Microsoft tutorial:
+    /// https://docs.microsoft.com/en-us/azure/active-directory-b2c/active-directory-b2c-devquickstarts-web-dotnet-susi
+    /// </summary>
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Claims()
+        {
+            var displayName =
+                ClaimsPrincipal.Current.FindFirst(ClaimsPrincipal.Current.Identities.First().NameClaimType);
+            ViewBag.DisplayName = displayName != null ? displayName.Value : string.Empty;
+            return View();
+        }
+    }
+}
